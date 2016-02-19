1a5e6b413cfe5aa164cd514c67777e61b74ec8e8
Hani Kazmi <hanikazmi@me.com>
2016-02-19 14:27:36 +0000

    Add syntax highlighting

---
+++ zsh/zsh-syntax-highlighting.zsh
@@ -0,0 +1,17 @@
+source ./zsh-syntax-highlighting/zsh-syntax-highlighting.zsh
+
+# Declare the variable
+typeset -A ZSH_HIGHLIGHT_STYLES
+
+# To have paths colored instead of underlined
+ZSH_HIGHLIGHT_STYLES[path]='fg=cyan,underline'
+
+# Have all commands in blue
+ZSH_HIGHLIGHT_STYLES[command]='fg=blue'
+ZSH_HIGHLIGHT_STYLES[alias]='fg=blue'
+ZSH_HIGHLIGHT_STYLES[builtin]='fg=blue'
+ZSH_HIGHLIGHT_STYLES[function]='fg=blue'
+
+# Have options in cyan
+ZSH_HIGHLIGHT_STYLES[single-hyphen-option]='fg=cyan'
+ZSH_HIGHLIGHT_STYLES[double-hyphen-option]='fg=cyan'
